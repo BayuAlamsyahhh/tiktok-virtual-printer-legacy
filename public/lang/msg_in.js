9a7ee2236b3c7ef4a8b76e5f30fa26c7834c703f
BayuAlamsyahhh <91405495+BayuAlamsyahhh@users.noreply.github.com>
2022-10-09 11:43:47 +0700

    Update msg_in.js

--- public/lang/msg_in.js
+++ public/lang/msg_in.js
@@ -1,3 +1,3 @@
 var MSG_GIFT = "terimaakasyy kepaadaa |username| yank uhdaa kirim gift";
 var MSG_WINNER = "selaamaaaaat kepaadaaaaa |username| kaarenaa berhasil menebaak jaawaabaan haa haa haaa";
-var MSG_TEST = "TEST SOOWAARAA";
+var MSG_TEST = "haloooo maaaas bayuuuuu";
